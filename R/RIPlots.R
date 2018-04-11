beb30256d8f740a12f99cd5a3110084c042443d9
John Reisner <johntreisner@gmail.com>
2018-04-10 21:16:12 -0500

    Variable y-axis, variable arguments

--- R/RIPlots.R
+++ R/RIPlots.R
@@ -1,23 +1,23 @@
 #' Plot each iteration's Rand Indices
 #'
 #' @param bicluster_obj A bicluster object.
-#' @param pch Point shape. This is equivalent to the `pch` argument for base graphics.
-#' @param cex Scale of the points in the plot.
-#' @param lwd Width of the smoothing spline line.
+#' @param ... Arguments to be passed to base R line and point graphics.
 #' @export
 #' @importFrom graphics points
 #' @return A plot
 
-RI_plots <- function(bicluster_obj, pch = 20, cex = 0.5, lwd = 1) {
+RI_plots <- function(bicluster_obj, ...) {
   model_df <- data.frame(PRI = as.vector(na.omit(bicluster_obj$RIs[, 1])),
                          QRI = as.vector(na.omit(bicluster_obj$RIs[, 2])))
   model_df$iteration <- seq_along(model_df$PRI)
 
-  plot(model_df$iteration, model_df$PRI, pch = pch, cex = cex,
-       ylab = "Rand Index", xlab = "Iteration", col = 2)
-  lines(model_df$iteration, model_df$PRI, col = 2, lwd = lwd)
-  points(model_df$iteration, model_df$QRI, col = 4, pch = pch, cex = cex)
-  lines(model_df$iteration, model_df$QRI, col = 4, lwd = lwd)
+  y_min <- floor(min(model_df) * 100) / 100
+
+  plot(model_df$iteration, model_df$PRI, ylim = c(y_min, 1),
+       ylab = "Rand Index", xlab = "Iteration", col = 2, ...)
+  lines(model_df$iteration, model_df$PRI, col = 2, ...)
+  points(model_df$iteration, model_df$QRI, col = 4, ...)
+  lines(model_df$iteration, model_df$QRI, col = 4, ...)
 
   legend("bottomright", c("P Rand Index", "Q Rand Index"), lty = rep(1, 2),
          pch = rep(20, 2), col = c(2, 4), lwd = rep(2, 2), cex = 0.8)
