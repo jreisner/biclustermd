59d19128d6e0981ae1589b697fd791b44017720f
John Reisner <johntreisner@gmail.com>
2018-04-13 19:40:16 -0500

    Variable names

--- R/globalvars.R
+++ R/globalvars.R
@@ -5,6 +5,7 @@
 NULL
 
 globalVariables(c("col_name_var", "val", "row_name_var", "row_proto", "col_proto",
+                  "row_group", "col_group",
                   "ColProto", "RowProto", "CellMSE", "cell_size", "value",
                   "plot_data", "v", "h", "Iteration", "SSE", "Rand Index", "Value"))
 
--- R/makeTrainingSet.R
+++ R/makeTrainingSet.R
@@ -1,5 +1,5 @@
 #' Make a data frame containing information about what cluster a row-column pair is in
-#' 
+#'
 #' @param data Data that was fed into the bicluster function.
 #' @param bicluster_obj A bicluster object.
 #' @export
@@ -9,38 +9,38 @@
 #' @return A data frame
 make_train_set <- function(data, bicluster_obj) {
   data <- as.data.frame(data)
-  
+
   bc <- bicluster_obj
   q <- bc$Q
   p <- bc$P
-  
+
   rownames(q) <- rownames(data)
   rownames(p) <- colnames(data)
-  
+
   rnames <- rownames(data)
-  
+
   mod_data <- data[, -1]
   mod_data$row_name_var <- rnames
-  
+
   mod_data <- mod_data[, c(ncol(mod_data), 1:(ncol(mod_data) - 1))]
   colnames(mod_data)[1] <- "row_name_var"
-  
+
   train_set <- mod_data %>%
     gather(col_name_var, val, -row_name_var)
- 
-  train_set$row_proto <- unlist(lapply(1:nrow(train_set), function (x) {
+
+  train_set$row_group <- unlist(lapply(1:nrow(train_set), function (x) {
     which(q[rownames(q) == train_set$row_name_var[x],] == 1)
   }
   ))
-  
-  train_set$col_proto <- unlist(lapply(1:nrow(train_set), function (x) {
+
+  train_set$col_group <- unlist(lapply(1:nrow(train_set), function (x) {
     which(p[rownames(p) == train_set$col_name_var[x],] == 1)
   }
   ))
-  
+
   train_set <- train_set %>%
-    arrange(row_proto, col_proto)
-  
+    arrange(row_group, col_group)
+
   return(train_set)
 }
 
