841be30b0710f1023cae36ca135a9c28215e1eb6
John Reisner <johntreisner@gmail.com>
2018-05-18 12:03:22 -0500

    Zoom in feature

--- R/ggbicluster.R
+++ R/ggbicluster.R
@@ -2,33 +2,45 @@
 #'
 #' @param bc_object A bicluster object.
 #' @param data The raw data that was biclustered.
-#' @param ... Arguments to be passed to `geom_vline()` and `geom_hline()`
+#' @param col_clusts A vector of column cluster indices to display. If NULL (default), all are displayed.
+#' @param row_clusts A vector of row cluster indices to display. If NULL (default), all are displayed.
 #' @param transform_colors If equals `TRUE` then the data is scaled by
 #'     `c` and run through a standard normal cdf before plotting. If `FALSE` (default), raw data
 #'     values are used in the heat map.
 #' @param c Value to scale the data by before running it through a standard normal CDF.
 #'     Default is 1/6.
+#' @param ... Arguments to be passed to `geom_vline()` and `geom_hline()`.
 #' @export
 #' @importFrom tidyr gather
 #' @importFrom ggplot2 ggplot aes geom_tile geom_hline geom_vline scale_fill_gradientn theme_bw theme
 #' @importFrom grDevices rainbow
 #' @return An object of class ggplot.
 
-gg_bicluster <- function(bc_object, data, ..., transform_colors = FALSE, c = 1/6) {
+gg_bicluster <- function(bc_object, data, col_clusts = NULL, row_clusts = NULL,
+                         transform_colors = FALSE, c = 1/6, ...) {
 
   bc <- bc_object
-
   P <- bc$P
   Q <- bc$Q
+
+  if(is.null(col_clusts)) {
+    col_clusts <- c(1:ncol(P))
+  }
+
+  if(is.null(row_clusts)) {
+    row_clusts <- c(1:ncol(Q))
+  }
 
   cols <- as.integer(part_matrix_to_vector(P))
   rows <- as.integer(part_matrix_to_vector(Q))
 
   p_list <- lapply(1:ncol(P), function(x) which(cols == x))
   p_list <- p_list[sapply(p_list, length) != 0]
+  p_list <- p_list[col_clusts]
 
   q_list <- lapply(1:ncol(Q), function(x) which(rows == x))
   q_list <- q_list[sapply(q_list, length) != 0]
+  q_list <- q_list[row_clusts]
 
   col_ord <- unlist(p_list)
   row_ord <- unlist(q_list)
