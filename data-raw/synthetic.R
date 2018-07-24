e1bce13ff71c088c519231a1a6109cbfb01df587
John Reisner <johntreisner@gmail.com>
2018-07-23 20:55:05 -0500

    Added sythetic data

---
+++ data-raw/synthetic.R
@@ -0,0 +1,11 @@
+A <- matrix(1:6, nrow = 2, ncol = 3)
+B <- matrix(5, nrow = 3, ncol = 4)
+synthetic <- kronecker(A, B)
+
+synthetic[sample(1:length(synthetic), 0.5 * length(synthetic))] <- NA
+
+shuffled_rows <- sample(1:nrow(synthetic), nrow(synthetic))
+shuffled_cols <- sample(1:ncol(synthetic), ncol(synthetic))
+
+synthetic <- synthetic[shuffled_rows, shuffled_cols]
+
