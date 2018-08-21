5b1c901c4d7a7b5522c4a95588fb30a9ec5b4705
John Reisner <johntreisner@gmail.com>
2018-08-20 20:49:32 -0500

    Examples

--- R/getRowNames.R
+++ R/getRowNames.R
@@ -8,19 +8,19 @@
 #'   cluster and \code{name} gives the row names in each cluster.
 #'
 #' @examples
-#' dat <- data("synthetic")
-#' rownames(dat) <- letters[1:nrow(dat)]
-#' colnames(dat) <- letters[1:col(dat)]
+#' data("synthetic")
+#' rownames(synthetic) <- letters[1:nrow(synthetic)]
+#' colnames(synthetic) <- letters[1:col(synthetic)]
 #' P01 <- partition_gen(12, 3)
 #' Q01 <- partition_gen(6, 2)
 #'
-#' bc <- bicluster(dat, P01, Q01, miss_val = mean(dat, na.rm = TRUE),
-#'                 miss_val_sd = sd(dat, na.rm = TRUE),
+#' bc <- bicluster(synthetic, P01, Q01, miss_val = mean(synthetic, na.rm = TRUE),
+#'                 miss_val_sd = sd(synthetic, na.rm = TRUE),
 #'                 col_min_num = 2, row_min_num = 2,
 #'                 col_num_to_move = 1, row_num_to_move = 1,
 #'                 max.iter = 10)
-#' row_cluster_names(bc, dat)
-#' col_cluster_names(bc, dat)
+#' row_cluster_names(bc, synthetic)
+#' col_cluster_names(bc, synthetic)
 row_cluster_names <- function(bc_object, data) {
   row_clust <- data.frame(row_cluster = part_matrix_to_vector(bc_object$Q))
 
