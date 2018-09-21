b59e6e853cb1e04b51b64ace56d0228999963cec
John Reisner <johntreisner@gmail.com>
2018-09-20 19:52:13 -0500

    Changed argument from 'i0' to 'i'

--- R/BinaryVectorGen.R
+++ R/BinaryVectorGen.R
@@ -1,11 +1,11 @@
 #' Make a binary vector with all values equal to zero except for one
 #'
 #' @param n Desired vector length.
-#' @param i0 Index whose value is one.
+#' @param i Index whose value is one.
 #' @return A vector
 
-binary_vector_gen <- function(n, i0){
+binary_vector_gen <- function(n, i){
   a <- rep(0, n)
-  a[i0] <- 1
+  a[i] <- 1
   return(a)
 }
