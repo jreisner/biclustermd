1a440a4c193208cb5cc9c8a2bc4039dc292a1ecb
John Reisner <johntreisner@gmail.com>
2018-09-25 20:16:43 -0500

    print method for biclustermd

--- R/print.R
+++ R/print.R
@@ -5,8 +5,12 @@
 #' @export
 
 print.biclustermd <- function(x, ...) {
-  cat("\n", x$iteration, "Iterations")
-  cat("\n", "Cell-Average Matrix:", "\n")
-  print(x$A)
+  cat("\n ", x$iteration, " Iterations", sep = "")
+  cat("\n ", "Initial SSE = ", format(round(x$InitialSSE), big.mark = ","),
+      "; Final SSE = ", format(round(x$SSE[x$iteration, 1]), big.mark = ","), sep = "")
+  cat("\n ", "Rand Indices: ", "P = ", round(x$RIs[x$iteration, 1], 3), ", Q = ",
+      round(x$RIs[x$iteration, 2], 3), sep = "")
+  cat("\n ", "Cell-Average Matrix:", "\n", sep = "")
+  print(data.frame(x$A))
   invisible(x)
 }
