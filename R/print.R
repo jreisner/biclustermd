b9d636ed235950acb3f2f91e8dec1b9ca293f181
John Reisner <johntreisner@gmail.com>
2018-10-02 21:24:55 -0500

    Fixed print method

--- R/print.R
+++ R/print.R
@@ -10,8 +10,7 @@ print.biclustermd <- function(x, ...) {
   cat("\n ", "Initial SSE = ", format(round(x$InitialSSE), big.mark = ","),
       "; Final SSE = ", format(round(x$SSE[x$iteration, 1]), big.mark = ","), sep = "")
   cat("\n ", "Rand Indices: ", "P = ", round(x$RIs[x$iteration, 1], 3), ", Q = ",
-      round(x$RIs[x$iteration, 2], 3), sep = "")
-  cat("\n ", "Cell-average matrix dimensions:")
-  print(dim(x$A))
+      round(x$RIs[x$iteration, 2], 3), "\n", sep = "")
+  print(paste0(prod(dim(x$data)), "values biclustered"))
   invisible(x)
 }
