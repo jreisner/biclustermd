39c93a53de87d7ce83f1b44892751bf1e67b3131
John Reisner <johntreisner@gmail.com>
2018-10-02 21:47:55 -0500

    Fixed ggplot bug

--- R/ggplot.R
+++ R/ggplot.R
@@ -1,10 +1,10 @@
 #' Plot the SSE or Rand Indices of a \code{biclustermd} object.
 #'
-#' @param data an object of class \code{biclustermd}. 
-#' @param mapping unused; included to match \code{ggplot2} generic 
+#' @param data an object of class \code{biclustermd}.
+#' @param mapping unused; included to match \code{ggplot2} generic
 #' @param value which value to plot. Can be either "sse" or "ri".
-#' @param ... unused; included to match \code{ggplot2} generic 
-#' @param environment unused; included to match \code{ggplot2} generic 
+#' @param ... unused; included to match \code{ggplot2} generic
+#' @param environment unused; included to match \code{ggplot2} generic
 #' @export
 #' @importFrom magrittr %>%
 #' @importFrom ggplot2 ggplot aes scale_colour_manual geom_line geom_point theme_bw scale_x_continuous
@@ -12,14 +12,14 @@
 #' @return A data frame
 ggplot.biclustermd <- function(data, mapping = NULL, value = c("sse", "ri"), ..., environment = NULL) {
   if(value == "sse") {
-    value_df <- data.frame(x$SSE)
-    
+    value_df <- data.frame(data$SSE)
+
     p <- value_df %>%
-      ggplot(aes(Iteration, SSE)) 
+      ggplot(aes(Iteration, SSE))
   } else if(value == "ri") {
-    value_df <- data.frame(x$RIs)
+    value_df <- data.frame(data$RIs)
     names(value_df)[-3] <- c("P", "Q")
-    
+
     p <- value_df %>%
       gather(`Rand Index`, Value, -Iteration) %>%
       ggplot(aes(Iteration, Value, colour = `Rand Index`)) +
@@ -30,5 +30,5 @@ ggplot.biclustermd <- function(data, mapping = NULL, value = c("sse", "ri"), ...
     geom_point() +
     theme_bw() +
     scale_x_continuous(breaks = 0:(x$iteration - 1))
-  
+
 }
