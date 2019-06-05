6aa4e2ac05012e791cefb49b6b156e84b31334f0
John Reisner <johntreisner@gmail.com>
2019-06-04 21:15:48 -0500

    ggplot method troubleshooting

--- R/ggplot.R
+++ R/ggplot.R
@@ -1,6 +1,6 @@
 #' Plot the SSE or Rand Indices of a \code{biclustermd} object.
 #'
-#' @param x an object of class \code{biclustermd}.
+#' @param data an object of class \code{biclustermd}.
 #' @param mapping unused; included to match \code{ggplot2} generic
 #' @param value which value to plot. Can be either "sse" or "similarity".
 #' @param ... unused; included to match \code{ggplot2} generic
@@ -10,27 +10,27 @@
 #' @importFrom ggplot2 ggplot aes scale_colour_manual geom_line geom_point theme_bw scale_x_continuous
 #' @importFrom tidyr gather
 #' @return A data frame
-ggplot.biclustermd <- function(x, mapping = NULL, value = c("sse", "similarity"), ..., environment = NULL) {
+ggplot.biclustermd <- function(data, mapping = NULL, value = c("sse", "similarity"), ..., environment = NULL) {
   if(value == "sse") {
-    value_df <- data.frame(x$SSE)
+    value_df <- data.frame(data$SSE)
 
     p <- value_df %>%
       ggplot(aes(Iteration, SSE))
   } else if(value == "similarity") {
-    value_df <- data.frame(x$Similarities)
-    if(x$params$similarity == 'Rand') {
-      value_df <- x$Similarities[, c("P_rand", "Q_rand", "Iteration")]
-    } else if(x$params$similarity == 'HA') {
-      value_df <- x$Similarities[, c("P_ha", "Q_ha", "Iteration")]
-    } else if(x$params$similarity == 'Jaccard') {
-      value_df <- x$Similarities[, c("P_jaccard", "Q_jaccard", "Iteration")]
+    value_df <- data.frame(data$Similarities)
+    if(data$params$similarity == 'Rand') {
+      value_df <- data$Similarities[, c("P_rand", "Q_rand", "Iteration")]
+    } else if(data$params$similarity == 'HA') {
+      value_df <- data$Similarities[, c("P_ha", "Q_ha", "Iteration")]
+    } else if(data$params$similarity == 'Jaccard') {
+      value_df <- data$Similarities[, c("P_jaccard", "Q_jaccard", "Iteration")]
     }
     names(value_df)[-3] <- c("Columns (P)", "Rows (Q)")
 
     p <- value_df %>%
-      gather(`Similarity Index`, Value, -Iteration) %>%
-      ggplot(aes(Iteration, Value, colour = `Similarity Index`)) +
-      scale_colour_manual(paste0(x$params$similarity, " Index"), values = c("red", "blue"))
+      gather(similarity_index, Value, -Iteration) %>%
+      ggplot(aes(Iteration, Value, colour = similarity_index)) +
+      scale_colour_manual(paste0(data$params$similarity, " Index"), values = c("red", "blue"))
   }
   p +
     geom_line() +
