178010cc82582ca5b380b42069f08d90171fa2f1
John Reisner <johntreisner@gmail.com>
2019-06-04 19:11:17 -0500

    Removed thousands separator

--- R/print.R
+++ R/print.R
@@ -7,8 +7,8 @@
 print.biclustermd <- function(x, ...) {
   cat("\n ", "Data has ", prod(dim(x$data)), " values, ", round(100 * sum(is.na(x$data)) / prod(dim(x$data)), 2), "% of which are missing", sep = "")
   cat("\n ", x$iteration, " Iterations", sep = "")
-  cat("\n ", "Initial SSE = ", format(round(x$InitialSSE), big.mark = ","),
-      "; Final SSE = ", format(round(x$SSE[x$iteration, 1]), big.mark = ","), sep = "")
+  cat("\n ", "Initial SSE = ", round(x$InitialSSE),
+      "; Final SSE = ", round(x$SSE[x$iteration, 1]), sep = "")
   cat("\n ", "Rand Indices: ", "P = ", round(x$RIs[x$iteration, 1], 3), ", Q = ",
       round(x$RIs[x$iteration, 2], 3), "\n", sep = "")
   invisible(x)
