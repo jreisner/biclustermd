ac16d0848e5b651f9ec3b40b7a08b06fc2e3e13d
John Reisner <johntreisner@gmail.com>
2019-06-20 20:50:23 -0500

    Removed default theme, color palette

--- R/MSEHeatmap.R
+++ R/MSEHeatmap.R
@@ -1,48 +1,35 @@
 #' Make a heatmap of cell MSEs
 #'
 #' @param x An object of class \code{biclustermd}.
-#' @param linewidth Width of vertical and horizontal lines. Default is 0.1.
-#' @param log_scale Logical. If TRUE, fill legend will be on a log10 scale. Default is FALSE.
+#' @param ... Arguments to pass to \code{geom_tile()}
 #' @export
 #' @importFrom magrittr %>%
 #' @importFrom dplyr mutate
-#' @importFrom ggplot2 ggplot aes geom_tile scale_fill_gradientn theme_bw labs
-#' @importFrom stats pnorm quantile
-#' @importFrom grDevices rainbow
+#' @importFrom ggplot2 ggplot aes geom_tile labs
+
 #' @return A ggplot object.
 #' @examples
 #' data("synthetic")
-#' P01 <- partition_gen(12, 3)
-#' Q01 <- partition_gen(6, 2)
-#'
-#' bc <- bicluster(synthetic, P01, Q01, miss_val = mean(synthetic, na.rm = TRUE),
+#' bc <- bicluster(synthetic, col_clusters = 3, row_clusters = 2,
+#'                 miss_val = mean(synthetic, na.rm = TRUE),
 #'                 miss_val_sd = sd(synthetic, na.rm = TRUE),
 #'                 col_min_num = 2, row_min_num = 2,
 #'                 col_num_to_move = 1, row_num_to_move = 1,
 #'                 max.iter = 10)
+#'
 #' mse_heatmap(bc)
+#'
+#' mse_heatmap(bc) + ggplot2::scale_fill_viridis_c()
 
-mse_heatmap <- function(x, linewidth = 0.1, log_scale = FALSE) {
+mse_heatmap <- function(x, ...) {
 
   mse_obj <- cell_mse(x)
 
-  gg <- mse_obj %>%
+  mse_obj %>%
     ggplot(aes(x = col_cluster, y = row_cluster, fill = CellMSE)) +
-    geom_tile(colour = "gray35", size = linewidth) +
-    theme_bw() +
+    geom_tile(...) +
     labs(x = "Column Cluster Index",
          y = "Row Cluster Index",
          fill = "Cell MSE")
 
-  if(log_scale == TRUE) {
-    gg <- gg +
-      scale_fill_gradientn(colours = rev(rainbow(250, start = 0, end = 0.7)),
-                           na.value = "white", trans = "log10")
-  } else {
-    gg <- gg +
-      scale_fill_gradientn(colours = rev(rainbow(250, start = 0, end = 0.7)),
-                           na.value = "white")
-  }
-
-  return(gg)
 }
