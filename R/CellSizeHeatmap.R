3673ed27134e798e859dab7c2811fea2a5012164
John Reisner <johntreisner@gmail.com>
2019-06-20 20:35:22 -0500

    Removed default theme, color palette

--- R/CellSizeHeatmap.R
+++ R/CellSizeHeatmap.R
@@ -1,28 +1,27 @@
 #' Make a heat map of bicluster cell sizes.
 #'
 #' @param x An object of class \code{biclustermd}.
-#' @param linewidth Width of vertical and horizontal lines. Default is 0.1.
-#' @param log_scale Logical. If TRUE, log10 scale on the legend is used. Default is FALSE.
+#' @param ... Arguments to pass to \code{geom_tile()}
 #' @export
 #' @importFrom magrittr %>%
 #' @importFrom dplyr mutate row_number
 #' @importFrom tidyr gather
-#' @importFrom ggplot2 ggplot aes geom_tile scale_fill_gradientn theme_bw theme labs element_blank
-#' @importFrom stats quantile
-#' @importFrom grDevices rainbow
+#' @importFrom ggplot2 ggplot aes geom_tile theme labs element_blank
 #' @examples
 #' data("synthetic")
-#' P01 <- partition_gen(12, 3)
-#' Q01 <- partition_gen(6, 2)
 #'
-#' bc <- bicluster(synthetic, P01, Q01, miss_val = mean(synthetic, na.rm = TRUE),
+#' bc <- bicluster(synthetic, col_clusters = 3, row_clusters = 2,
+#'                 miss_val = mean(synthetic, na.rm = TRUE),
 #'                 miss_val_sd = sd(synthetic, na.rm = TRUE),
 #'                 col_min_num = 2, row_min_num = 2,
 #'                 col_num_to_move = 1, row_num_to_move = 1,
 #'                 max.iter = 10)
+#'
 #' cell_heatmap(bc)
+#'
+#' cell_heatmap(bc) + ggplot2::scale_fill_viridis_c()
 
-cell_heatmap <- function(x, linewidth = 0.1, log_scale = FALSE) {
+cell_heatmap <- function(x, ...) {
 
   bc <- x
 
@@ -34,25 +33,12 @@ cell_heatmap <- function(x, linewidth = 0.1, log_scale = FALSE) {
     mutate(row_proto = row_number()) %>%
     gather(col_proto, cell_size, -row_proto)
 
-  gg <- cell_sizes %>%
+  cell_sizes %>%
     ggplot(aes(x = col_proto, y = row_proto, fill = cell_size)) +
-    geom_tile(colour = "grey30", size = linewidth) +
-    theme_bw() +
+    geom_tile(...) +
     theme(panel.grid.major = element_blank(),
           panel.grid.minor = element_blank()) +
     labs(x = "Column Cluster Index", y = "Row Cluster Index",
          fill = "Cell Size")
 
-
-  if(log_scale == TRUE) {
-    gg <- gg +
-      scale_fill_gradientn(colours = rev(rainbow(250, start = 0, end = 0.7)),
-                           na.value = "white", trans = "log10")
-  } else {
-    gg <- gg +
-      scale_fill_gradientn(colours = rev(rainbow(250, start = 0, end = 0.7)),
-                           na.value = "white")
-  }
-
-  return(gg)
 }
