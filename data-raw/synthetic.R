f932db710c741ce58c90a49962260e67e97ccaa5
John Reisner <johntreisner@gmail.com>
2019-07-10 19:20:25 -0500

    Added row, column names

--- data-raw/synthetic.R
+++ data-raw/synthetic.R
@@ -1,6 +1,9 @@
+set.seed(29)
 A <- matrix(1:6, nrow = 2, ncol = 3)
 B <- matrix(5, nrow = 3, ncol = 4)
 synthetic <- kronecker(A, B)
+rownames(synthetic) <- paste0("R", 1:6)
+colnames(synthetic) <- paste0("C", 1:12)
 
 synthetic[sample(1:length(synthetic), 0.5 * length(synthetic))] <- NA
 
@@ -8,4 +11,3 @@ shuffled_rows <- sample(1:nrow(synthetic), nrow(synthetic))
 shuffled_cols <- sample(1:ncol(synthetic), ncol(synthetic))
 
 synthetic <- synthetic[shuffled_rows, shuffled_cols]
-
