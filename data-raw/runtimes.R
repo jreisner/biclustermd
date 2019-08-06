20080c270b14cd82de18c4992f331a213503c26a
John Reisner <johntreisner@gmail.com>
2019-08-05 19:40:38 -0500

    Add unique identifier of parameter combination

--- data-raw/runtimes.R
+++ data-raw/runtimes.R
@@ -1,3 +1,6 @@
 runtimes <- read.csv("data-raw/runtimes.csv")
 
+runtimes$combination_no <- rep(1:80, each = 30)
+runtimes <- runtimes[, c(13, 1:12)]
+
 usethis::use_data(runtimes, overwrite = TRUE)
