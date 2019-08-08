14fa9be3e9cf27ab95f0d5e181a859dd222cd79f
John Reisner <johntreisner@gmail.com>
2019-08-07 19:24:52 -0500

    Added examples

--- R/col.names.biclustermd.R
+++ R/col.names.biclustermd.R
@@ -1,10 +1,21 @@
 #' Get data matrix column names and their corresponding column cluster membership
-#' 
+#'
 #' @param x and object of class \code{biclustermd}
-#' 
+#'
 #' @return a data frame with column names of the shuffled matrix and corresponding column cluster names.
-#' 
+#'
 #' @export
+#'
+#' @examples
+#' data("synthetic")
+#' # default parameters
+#' bc <- biclustermd(synthetic)
+#' bc
+#' col.names(bc)
+#' # this is a simplified version of the output for gather(bc):
+#' library(dplyr)
+#' gather(bc) %>% distinct(col_group, col_name)
+
 col.names.biclustermd <- function(x) {
   col_clust <- data.frame(col_cluster = part_matrix_to_vector(x$P))
   col_clust$name <- colnames(x$data)
--- R/row.names.R
+++ R/row.names.R
@@ -1,10 +1,20 @@
 #' Get data matrix row names and their corresponding row cluster membership
-#' 
+#'
 #' @param x and object of class \code{biclustermd}
-#' 
+#'
 #' @return a data frame with row names of the shuffled matrix and corresponding row cluster names.
-#' 
+#'
 #' @export
+#'
+#' @examples
+#' data("synthetic")
+#' # default parameters
+#' bc <- biclustermd(synthetic)
+#' bc
+#' row.names(bc)
+#' # this is a simplified version of the output for gather(bc):
+#' library(dplyr)
+#' gather(bc) %>% distinct(row_group, row_name)
 
 row.names.biclustermd <- function (x) {
   row_clust <- data.frame(row_cluster = part_matrix_to_vector(x$Q))
