64cc68e039abaed657a8dd8857ab7d8855c43924
John Reisner <johntreisner@gmail.com>
2019-08-07 19:00:47 -0500

    Ensure data is list is that actual data, not just the name

--- R/rep_biclustermd.R
+++ R/rep_biclustermd.R
@@ -1,8 +1,8 @@
 #' Repeat a biclustering to achieve a minimum SSE solution
-#' 
+#'
 #' @param data Dataset to bicluster. Must to be a data matrix with only numbers and missing values in the data set. It should have row names and column names.
 #' @param nrep The number of times to repeat the biclustering. Default 10.
-#' @param parallel Logical indicating if the user would like to utilize the 
+#' @param parallel Logical indicating if the user would like to utilize the
 #'     \code{foreach} parallel backend. Default is FALSE.
 #' @param ncores The number of cores to use if parallel computing. Default 2.
 #' @param col_clusters The number of clusters to partition the columns into.
@@ -31,7 +31,7 @@
 #' data("synthetic")
 #'
 #' # 20 repeats without parallelization
-#' repeat_bc <- rep_biclustermd(synthetic, nrep = 20, 
+#' repeat_bc <- rep_biclustermd(synthetic, nrep = 20,
 #'                              col_clusters = 3, row_clusters = 2,
 #'                              miss_val = mean(synthetic, na.rm = TRUE),
 #'                              miss_val_sd = sd(synthetic, na.rm = TRUE),
@@ -42,7 +42,7 @@
 #' autoplot(repeat_bc$best_bc)
 #' plot(repeat_bc$rep_sse, type = 'b', pch = 20)
 #' repeat_bc$runtime
-#' 
+#'
 #' # 20 repeats with parallelization over 2 cores
 #' repeat_bc <- rep_biclustermd(synthetic, nrep = 20, parallel = TRUE, ncores = 2,
 #'                              col_clusters = 3, row_clusters = 2,
@@ -56,67 +56,68 @@
 
 rep_biclustermd <- function(data, nrep = 10, parallel = FALSE, ncores = 2,
                             col_clusters = floor(sqrt(ncol(data))),
-                            row_clusters = floor(sqrt(nrow(data))), 
-                            miss_val = mean(data, na.rm = TRUE), 
-                            miss_val_sd = 1, similarity = "Rand", 
-                            row_min_num = 5, col_min_num = 5, 
+                            row_clusters = floor(sqrt(nrow(data))),
+                            miss_val = mean(data, na.rm = TRUE),
+                            miss_val_sd = 1, similarity = "Rand",
+                            row_min_num = 5, col_min_num = 5,
                             row_num_to_move = 1, col_num_to_move = 1,
                             row_shuffles = 1, col_shuffles = 1, max.iter = 100) {
   mcall <- as.list(match.call())
   mcall <- mcall[-1]
   mcall <- mcall[!(names(mcall) %in% c("nrep", "parallel", "ncores", "verbose"))]
-  
+  mcall$data <- data
+
   if(!parallel) {
-    
+
     st <- proc.time()
     best_sse <- .Machine$double.xmax
     sse <- numeric(nrep)
     for(i in 1:nrep) {
-      
+
       bc <- do.call(biclustermd, mcall)
-      
+
       sse[i] <- bc$SSE[bc$iteration, 1]
-      
+
       if(sse[i] < best_sse) {
-        
+
         best_sse <- sse[i]
         best_bc <- bc
-        
+
       }
     }
     et <- proc.time()
-    
+
     list(
       best_bc = best_bc,
       rep_sse = sse,
       runtime = et - st
     )
-    
+
   } else if(parallel) {
-    
+
     st <- proc.time()
     cl <- makeCluster(ncores)
     registerDoParallel(cl)
-    
+
     results <- foreach(i = 1:nrep, .packages = 'biclustermd') %dopar% {
-      
+
       do.call(biclustermd, mcall)
-      
+
     }
-    
+
     stopCluster(cl)
-    
+
     sse <- sapply(results, function(z) z$SSE[z$iteration, 1])
     best_bc <- results[[which.min(sse)]]
     rm(results)
     et <- proc.time()
-    
+
     list(
       best_bc = best_bc,
       rep_sse = sse,
       runtime = et - st
     )
-    
+
   }
-  
+
 }
